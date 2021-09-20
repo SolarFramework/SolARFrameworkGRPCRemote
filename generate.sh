bb2097835ccebbcb1853c8b1658142afc9bc97f1
Jerome <Royan>
2021-09-20 14:49:40 +0200

    fix path in generate.sh script

--- generate.sh
+++ generate.sh
@@ -7,5 +7,5 @@ echo "PATH=$PATH"
 
 #TODO generate compilation database for the SolAR Framework
 
-$XPCF_MODULE_ROOT/xpcf_grpc_gen/1.0.0/bin/x86_64/static/release/xpcf_grpc_gen -n SolARFramework -v 0.10.0 -r SolARBuild@github -u https://github.com/SolarFramework/SolARFramework --database_dir ~/Dev/SolAR/core/build-SolARFramework-Desktop_Qt_5_15_2_GCC_64bit-Release/ --std c++1z --remove_comments_in_macro -g protobuf -o ~/Dev/SolAR/SolARFramework/core/SolARFrameworkGRPCRemote
+$XPCF_MODULE_ROOT/xpcf_grpc_gen/1.0.0/bin/x86_64/static/release/xpcf_grpc_gen -n SolARFramework -v 0.10.0 -r SolARBuild@github -u https://github.com/SolarFramework/SolARFramework --database_dir ~/Dev/SolAR/core/build-SolARFramework-Desktop_Qt_5_15_2_GCC_64bit-Release/ --std c++1z --remove_comments_in_macro -g protobuf -o ~/Dev/SolAR/core/SolARFrameworkGRPCRemote
 
