c7fe5bca09977d4c040853b98e5e4069b2d4a5b6
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2022-05-18 16:06:37 +0200

    Fix #include path (remove absolute path)

--- IMappingPipeline_grpcServer.h
+++ IMappingPipeline_grpcServer.h
@@ -2,7 +2,7 @@
 
 #ifndef IMAPPINGPIPELINE_GRPCSERVER_H
 #define IMAPPINGPIPELINE_GRPCSERVER_H
-#include "/mnt/c/Users/nduong/bcom/projects/Argo/SolARFramework/SolAR/core/SolARFramework/interfaces/api/pipeline/IMappingPipeline.h"
+#include "api/pipeline/IMappingPipeline.h"
 #include <xpcf/component/ConfigurableBase.h>
 #include <xpcf/remoting/IGrpcService.h>
 #include <xpcf/remoting/GrpcHelper.h>
