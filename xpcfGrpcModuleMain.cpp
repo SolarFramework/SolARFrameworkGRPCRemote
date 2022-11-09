9f4b2eedecaa2a7b17d4ea6e2da9105e90525cd5
Jerome Royan <jerome.royan@b-com.com>
2022-11-09 14:24:28 +0100

    set UUID of SolARFrameworkGRPCRemote to a0f522d1-b70e-4d0f-ad78-84e78a9af6bf

--- xpcfGrpcModuleMain.cpp
+++ xpcfGrpcModuleMain.cpp
@@ -1,7 +1,7 @@
 #include "xpcfGrpcModuleMain.h"
 #include <xpcf/module/ModuleFactory.h>
 namespace xpcf=org::bcom::xpcf;
-XPCF_DECLARE_MODULE("084a0405-6483-4bd2-8115-993b634533fa", "xpcfGrpcRemotingSolARFramework","xpcf remoting module for project SolARFramework");
+XPCF_DECLARE_MODULE("a0f522d1-b70e-4d0f-ad78-84e78a9af6bf", "xpcfGrpcRemotingSolARFramework","xpcf remoting module for project SolARFramework");
 
 extern "C" XPCF_MODULEHOOKS_API xpcf::XPCFErrorCode XPCF_getComponent(const boost::uuids::uuid& componentUUID,SRef<xpcf::IComponentIntrospect>& interfaceRef)
 {
