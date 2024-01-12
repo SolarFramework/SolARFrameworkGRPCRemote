9721341627378566ade586dd05c921ace8b21212
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2024-01-12 16:18:35 +0100

    feat(license): replace Apache 2.0 by b<>com proprietary license

--- generate.sh
+++ generate.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
+
+###############################################################################
+#  SolAR ARCloud
+#  (C) Copyright 2021 b<>com. All rights reserved.
 #
-# @copyright Copyright (c) 2021 B-com http://www.b-com.com/
+#  IDDN: FR.001.020021.000.S.C.2024.000.00000
+#  This software is the confidential intellectual property of b<>com.
+#  You shall not disclose it and shall use it only in accordance with
+#  the terms of the license agreement you entered into with b<>com.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+################################################################################/
 
 set -e
 
